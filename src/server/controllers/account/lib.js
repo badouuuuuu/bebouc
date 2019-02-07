8345852946739e4fbd9879b80d1dc6f5c33704e6
Laura <l.belferroum@gmail.com>
2019-02-07 16:07:45 +0100

    try to delete email

--- src/server/controllers/account/lib.js
+++ src/server/controllers/account/lib.js
@@ -5,7 +5,7 @@ const signup = (req, res) => {
     if (!req.body.email || !req.body.password) {
         // In case nothing is submitted
         res.status(400).json({
-            text: "Requête invalide",
+            text: "Invalid request",
         });
     } else {
         let user = {
@@ -25,7 +25,7 @@ const signup = (req, res) => {
                             reject(500);
                         } else if (result) {
                             res.status(204).json({
-                                text: "Content Vide",
+                                text: "Empty content",
                             });
                         } else {
                             resolve(true);
@@ -41,11 +41,11 @@ const signup = (req, res) => {
                 _u.save((err, usr) => {
                     if (err) {
                         res.status(500).json({
-                            text: "Erreur interne",
+                            text: "Internal error",
                         });
                     } else {
                         res.status(200).json({
-                            text: "Succes",
+                            text: "Success",
                             token: usr.getToken(),
                         });
                     }
@@ -55,17 +55,17 @@ const signup = (req, res) => {
                 switch (error) {
                     case 500:
                         res.status(500).json({
-                            text: "Erreur interne",
+                            text: "Internal error",
                         });
                         break;
                     case 204:
                         res.status(204).json({
-                            text: "L'adresse email existe déjà",
+                            text: "Mail adress already exist",
                         });
                         break;
                     default:
                         res.status(500).json({
-                            text: "Erreur interne",
+                            text: "Internal error",
                         });
                 }
             },
@@ -77,7 +77,7 @@ const login = (req, res) => {
     if (!req.body.email || !req.body.password) {
         // Le cas où l'email ou bien le password ne serait pas soumit ou nul
         res.status(400).json({
-            text: "Requête invalide",
+            text: "Invalid request",
         });
     } else {
         User.findOne(
@@ -87,37 +87,57 @@ const login = (req, res) => {
             (err, user) => {
                 if (err) {
                     res.status(500).json({
-                        text: "Erreur interne",
+                        text: "Internal error",
                     });
                 } else if (!user) {
                     res.status(401).json({
-                        text: "L'utilisateur n'existe pas",
+                        text: "User does not exist",
                     });
                 } else if (user.authenticate(req.body.password)) {
                     res.status(200).json({
                         token: user.getToken(),
-                        text: "Authentification réussi",
+                        text: "You're in baby",
                     });
                 } else {
                     res.status(401).json({
-                        text: "Mot de passe incorrect",
+                        text: "Password error",
                     });
                 }
             },
         );
     }
 };
-//
-// const delete = (req, res) => {
-//
-//     db.collection('users').findOneAndDelete({email: req.body.email},
+
+const deleteuser = req => {
+    let querymail = {email: req.body.email};
+
+    User.findOneAndDelete(querymail, (err, res) => {
+        res.status(201).send(res);
+        console.log("email delete");
+    });
+};
+//   User.deleteOne({email: req.body.email},
 //     (err, result) => {
-//       if (err) return res.send(500, err)
-//       res.send('The user is gone. Adios.')
-//     })
-//   };
+//     if (err){
+//
+//     return res.send(500, err)
+//     }
+//
+//     else{
+//
+//     res.send('A darth vader quote got deleted')
+//   }
+//   })
+//
+//     .then(res => {
+//     if (res.ok) return res.send(res)
+//   }).catch(err => {
+//     res.send(500, err)
+//   })
+//
 
 // On exporte nos deux fonctions
 
 exports.login = login;
 exports.signup = signup;
+exports.deleteuser = deleteuser;
