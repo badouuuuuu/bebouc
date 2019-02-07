1b37907fff92106babf73dce7d842163e612ce16
Elias <elias.djenidi@gmail.com>
2019-02-07 10:20:50 +0100

    post merge

--- src/server/routes/register.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-let Router = express.Router();
-
-Router.post("/register", (req, res) => {
-    res.send(`✔️ You have requested the POST register API ®️`);
-});
-
-module.exports = Router;
