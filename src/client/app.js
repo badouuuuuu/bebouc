6d885ef5cea51521c80fc8211ce77492149bd4a2
Elias <elias.djenidi@gmail.com>
2019-02-08 16:07:34 +0100

    infrastructure router front-end

--- src/client/app.js
+++ src/client/app.js
@@ -1,7 +1,25 @@
-import * as React from "react";
-import ReactDOM from "react-dom";
-import "./app.scss";
 
-import Homepage from "./components/homepage";
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Homepage } from './components/Homepage.js';
+import { Login } from './components/authentification/Login.js';
+import { Signup } from './components/authentification/Signup.js';
+import { PrivateRoute } from './components/PrivateRoute.js';
+import './App.css';
 
-ReactDOM.render(<Homepage />, document.querySelector("#app"));
+class App extends Component {
+        render() {
+        return (
+        <div className="App">
+            <div className="App-content">
+                <Switch>
+                    <Route exact path="/" component={Login}/>
+                    <Route exact path ="/signup" component={Signup}/>
+                    <PrivateRoute path='/homepage' component={Homepage} />
+                </Switch>
+            </div>
+        </div>
+        );
+    }
+}
+export default App;
