ec8f0a5f51ac2c293153771a7f33e2b44bb7b686
olivierk95 <keutgensolivier@gmail.com>
2019-02-08 14:59:48 +0100

    début de Sass

--- src/client/components/main.js
+++ src/client/components/main.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-export default class Homepage extends React.Component {
+export default class Main extends React.Component {
     render() {
         return (
             <div className="backgroundhome">
