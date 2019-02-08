318c1951681a87e4346938b58bb32e05012c6346
Elias <elias.djenidi@gmail.com>
2019-02-08 13:49:42 +0100

    tentative de fix npm pour bcrypt

--- src/server/controllers/reviewController.js
+++ src/server/controllers/reviewController.js
@@ -93,7 +93,7 @@ const destroy = (req, res) => {
         .catch(err => res.status(404).json(err));
 };
 
-// Exports des fonctions
+// Exporting methods
 exports.show = show;
 exports.create = create;
 exports.store = store;
