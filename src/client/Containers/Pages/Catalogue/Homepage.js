194f8767b218925015158758504052a1df752e2a
Elias <elias.djenidi@gmail.com>
2019-02-12 12:10:31 +0100

    réorganisation fichiers

--- src/client/Containers/Pages/Catalogue/Homepage.js
+++ src/client/Containers/Pages/Catalogue/Homepage.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import axios from "axios";
 import NavCatalogue from "./NavCatalogue";
-import NavBar from "../../../components/navbar";
+import NavBar from "../../Components/Navbar";
 
 export class Homepage extends React.Component {
     state = {
