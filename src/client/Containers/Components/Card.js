891142cfd6b5957a40ee230ad97244c9cb4ea2a2
Laura <l.belferroum@gmail.com>
2019-02-14 14:01:27 +0100

    userblack svg

---
+++ src/client/Containers/Components/Card.js
@@ -0,0 +1,48 @@
+import * as React from "react";
+import Modal from "./Modal";
+
+export class Card extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {showModal: ""};
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState({showModal: "is-active"});
+    }
+
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li>
+                        <h1>{`Book card number ${this.props.index}`}</h1>
+                    </li>
+                    <li>{`Title : ${this.props.title}`}</li>
+                    <li>{`Author : ${this.props.author}`}</li>
+                    <li>
+                        <button type="button" onClick={this.handleClick}>
+                            {"See book"}
+                        </button>
+                    </li>
+                    <li>
+                        {" "}
+                        <Modal
+                            show={this.state.showModal}
+                            index={this.props.index}
+                            key={this.props._id}
+                            title={this.props.title}
+                            author={this.props.author}
+                            language={this.props.language}
+                            owner={this.props.owner}
+                            summary={this.props.summary}
+                        />
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Card;
