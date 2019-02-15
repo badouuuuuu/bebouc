d104c6644dfa58306788efc0706103ba2f306ff0
Laura <l.belferroum@gmail.com>
2019-02-15 16:45:52 +0100

    new review component

---
+++ src/client/Containers/Components/reviews.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+// import Magazine from '../../../assets/open-magazine.svg';
+
+export class Reviews extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <form>
+                    <input />
+
+                    <button> {"Into the wild"}</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Reviews;
