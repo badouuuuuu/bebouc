19e1ef3a7dce469ad8d06afd3931fa63eb88cf74
Laura <l.belferroum@gmail.com>
2019-02-17 15:41:29 +0100

    tentative review fonction

--- src/client/Containers/Components/reviews.js
+++ src/client/Containers/Components/reviews.js
@@ -1,14 +1,53 @@
 import * as React from "react";
+import axios from "axios";
+
 // import Magazine from '../../../assets/open-magazine.svg';
 
 export class Reviews extends React.Component {
-    render() {
-        return (
-            <div className="container">
-                <form>
-                    <input />
+    constructor(props) {
+        super(props);
+        this.state = {
+            review: [],
+        };
+    }
 
-                    <button> {"Into the wild"}</button>
+    componentDidMount() {
+        axios
+            .get(`http://localhost/api/reviews/${this.props.bookid}`)
+            .then(res => {
+                const review = res.data;
+
+                this.setState({review});
+            });
+    }
+
+    render() {
+        const displayEachReviews = this.state.review.map((review, i) => {
+            let index = i + 1;
+
+            return <div author={review.authorName} review={review.comment} />;
+        });
+
+        return (
+            <div className="reviewcont">
+                <h1 className="titlereview"> {"Reviews"}</h1>
+                <br />
+                <br />
+                <br />
+
+                <form>
+                    <div className="Reviews">{displayEachReviews}</div>
+
+                    <input
+                        className="InsideReview"
+                        placeholder="Your review here"
+                    />
+                    <div>
+                        <button className="submit-button">
+                            {" "}
+                            {"Into the wild"}
+                        </button>
+                    </div>
                 </form>
             </div>
         );
