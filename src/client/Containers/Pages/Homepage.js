c1440813310009f60316070217dac69748bbc9f7
badou <badou@pop-os.localdomain>
2019-02-18 13:15:39 +0100

    fix catalogue junior

--- src/client/Containers/Pages/Homepage.js
+++ src/client/Containers/Pages/Homepage.js
@@ -2,38 +2,12 @@ import * as React from "react";
 // import axios from "axios";
 import HomepageAdmin from "./Admin/HomepageAdmin";
 import HomepageJunior from "./Catalogue/HomepageJunior";
-import axios from "axios";
 
 export class Homepage extends React.Component {
-    state = {
-        isAdmin: false,
-    };
-
-    componentWillMount() {
-        axios
-            .get("http://localhost/api/auth")
-            .then(response => {
-                console.log(response.data);
-
-                if (response.data.admin === true) {
-                    this.setState({
-                        isAdmin: false,
-                    });
-                } else {
-                    this.setState({
-                        isAdmin: true,
-                    });
-                }
-            })
-            .then(error => {
-                console.log(error);
-            });
-    }
-
     render() {
         let Display = null;
 
-        if (this.state.isAdmin) {
+        if (!this.props.isJunior) {
             Display = <HomepageJunior />;
         } else {
             Display = <HomepageAdmin />;
