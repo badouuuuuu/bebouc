2d2f34578789f712006fb5aaee7d1968fbf59219
Elias <elias.djenidi@gmail.com>
2019-02-18 10:47:46 +0100

    put Add a User into a Modal which is clickable open/close

--- src/client/Containers/Pages/Authentification/Register.js
+++ src/client/Containers/Pages/Authentification/Register.js
@@ -81,102 +81,128 @@ export class Register extends React.Component {
 
     render() {
         return (
-            <div className="container">
-                <section className="section_addusers_title">
-                    <h1 className="addusers_title"> {" Add New User"} </h1>{" "}
-                </section>{" "}
-                <div className="adduser-component">
-                    <form onSubmit={this.handleSubmit}>
-                        <i className="far fa-envelope icon-input-field" />
-                        <label id="icon-input-field" htmlFor="email" />
-                        <input
-                            className={this.state.error_input}
-                            type="text"
-                            name="email"
-                            id="email"
-                            placeholder="email"
-                            value={this.state.email}
-                            onChange={this.handleChange}
-                            required
-                        />
-                        <br />
-                        <i className="far fa-user icon-input-field" />
-                        <label id="icon-input-field" htmlFor="name" />
-                        <input
-                            className="input_addusers is-small"
-                            type="text"
-                            name="name"
-                            id="name"
-                            placeholder="Name"
-                            value={this.state.name}
-                            onChange={this.handleChange}
-                            required
-                        />
-                        <br />
-                        <i className="fas fa-signature icon-input-field" />
-                        <label id="icon-input-field" htmlFor="surname" />
-                        <input
-                            className="input_addusers is-small"
-                            type="text"
-                            name="surname"
-                            id="surname"
-                            placeholder="Surname"
-                            value={this.state.surname}
-                            onChange={this.handleChange}
-                            required
-                        />
-                        <br />
-                        <i className="fas fa-key icon-input-field" />
-                        <label id="icon-input-field" htmlFor="password" />
-                        <input
-                            className="input_addusers is-small"
-                            type="password"
-                            name="password"
-                            id="password"
-                            placeholder="password"
-                            value={this.state.password}
-                            onChange={this.handleChange}
-                            required
-                        />
-                        <br />
-                        <br />
-                        <label htmlFor="admin"> {" Coach "} </label>{" "}
-                        <input
-                            className="checkbox"
-                            type="checkbox"
-                            name="admin"
-                            value={this.state.admin}
-                            onChange={this.handleChange}
-                            checked={this.state.admin}
-                        />
-                        <br />
-                        <p className="error">
-                            {" "}
-                            {this.state.error_alreadyexist}{" "}
-                        </p>{" "}
-                        <button
-                            disabled={
-                                !this.state.email &&
-                                !this.state.name &&
-                                !this.state.surname &&
-                                !this.state.password
-                            }
-                            type="submit"
-                            className={this.state.addusers_button}>
-                            {" "}
-                            {this.state.addusers_success}{" "}
-                        </button>{" "}
-                    </form>{" "}
-                    <hr />
-                    <a href="/">
-                        {" "}
-                        <button type="submit" className="secondary-button">
-                            {" "}
-                            {"Back"}{" "}
-                        </button>{" "}
-                    </a>{" "}
-                </div>{" "}
+            <div className={`modal ${this.props.show}`}>
+                <div className="modal-background" />
+                <div className="modal-content">
+                    <div className="container">
+                        <section className="section_addusers_title">
+                            <h1 className="addusers_title">
+                                {" "}
+                                {" Add New User"}{" "}
+                            </h1>{" "}
+                        </section>{" "}
+                        <div className="adduser-component">
+                            <form onSubmit={this.handleSubmit}>
+                                <i className="far fa-envelope icon-input-field" />
+                                <label id="icon-input-field" htmlFor="email" />
+                                <input
+                                    className={this.state.error_input}
+                                    type="text"
+                                    name="email"
+                                    id="email"
+                                    placeholder="email"
+                                    value={this.state.email}
+                                    onChange={this.handleChange}
+                                    required
+                                />
+                                <br />
+                                <i className="far fa-user icon-input-field" />
+                                <label id="icon-input-field" htmlFor="name" />
+                                <input
+                                    className="input_addusers is-small"
+                                    type="text"
+                                    name="name"
+                                    id="name"
+                                    placeholder="Name"
+                                    value={this.state.name}
+                                    onChange={this.handleChange}
+                                    required
+                                />
+                                <br />
+                                <i className="fas fa-signature icon-input-field" />
+                                <label
+                                    id="icon-input-field"
+                                    htmlFor="surname"
+                                />
+                                <input
+                                    className="input_addusers is-small"
+                                    type="text"
+                                    name="surname"
+                                    id="surname"
+                                    placeholder="Surname"
+                                    value={this.state.surname}
+                                    onChange={this.handleChange}
+                                    required
+                                />
+                                <br />
+                                <i className="fas fa-key icon-input-field" />
+                                <label
+                                    id="icon-input-field"
+                                    htmlFor="password"
+                                />
+                                <input
+                                    className="input_addusers is-small"
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    placeholder="password"
+                                    value={this.state.password}
+                                    onChange={this.handleChange}
+                                    required
+                                />
+                                <br />
+                                <br />
+                                <label htmlFor="admin">
+                                    {" "}
+                                    {" Coach "}{" "}
+                                </label>{" "}
+                                <input
+                                    className="checkbox"
+                                    type="checkbox"
+                                    name="admin"
+                                    value={this.state.admin}
+                                    onChange={this.handleChange}
+                                    checked={this.state.admin}
+                                />
+                                <br />
+                                <p className="error">
+                                    {" "}
+                                    {this.state.error_alreadyexist}{" "}
+                                </p>{" "}
+                                <button
+                                    disabled={
+                                        !this.state.email &&
+                                        !this.state.name &&
+                                        !this.state.surname &&
+                                        !this.state.password
+                                    }
+                                    type="submit"
+                                    className={this.state.addusers_button}>
+                                    {" "}
+                                    {this.state.addusers_success}{" "}
+                                </button>{" "}
+                            </form>{" "}
+                            <hr />
+                            <a href="/">
+                                {" "}
+                                <button
+                                    type="submit"
+                                    className="secondary-button">
+                                    {" "}
+                                    {"Back"}{" "}
+                                </button>{" "}
+                            </a>{" "}
+                        </div>{" "}
+                    </div>
+                </div>
+                <button
+                    onClick={this.props.close}
+                    className="modal-close is-large"
+                    aria-label="close"
+                />
             </div>
         );
     }
 }
+
+export default Register;
