71f2729056e5972f71a8e460e2a8de1e330a2404
Laura <l.belferroum@gmail.com>
2019-02-18 09:50:26 +0100

    début addbook

--- src/client/Containers/Pages/Catalogue/AddBook.js
+++ src/client/Containers/Pages/Catalogue/AddBook.js
@@ -164,26 +164,6 @@ export class AddBook extends React.Component {
                     </button>
                 </a>
                 <hr />
-                <h1>{"Books database list : "}</h1>
-                <ul>
-                    {this.state.books.map(book => (
-                        <li key={book._id}>
-                            {"-----------------"}
-                            <br />
-                            <strong>{"Title : "} </strong>
-                            {book.title} <br />
-                            <strong> {"Author : "} </strong>
-                            {book.author} <br />
-                            <strong>{"ISBN : "} </strong> {book.isbn} <br />
-                            <strong>{"language : "}</strong> {book.language}{" "}
-                            <br />
-                            <strong> {"summary : "}</strong> {book.summary}{" "}
-                            <br />
-                            <strong>{"owner : "} </strong> {book.owner} <br />
-                            <strong>{"Database Id : "} </strong> {book._id}
-                        </li>
-                    ))}
-                </ul>
             </div>
         );
     }
